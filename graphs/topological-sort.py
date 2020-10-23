99b3e92e821ee3d6ca89ec3b9cd68d9f0ecf5d0e
dinmukhamed <dinmuhamed93@gmail.com>
2020-10-23 18:27:04 +0600

    commit

--- graphs/topological-sort.py
+++ graphs/topological-sort.py
@@ -19,17 +19,17 @@ def topologicalSort(edges, V):
     for i in range(V):
         if not visited[i]:
             topologicalUtil(i)
-    
+
     stack.reverse()
     print(stack)
 
+
 def driver():
-    edges = [[0,1],[0,5],[1,7],[3,2],[3,8],[3,4],[4,8],[6,0],[6,1],[8,2],[9,4]]
+    edges = [[0, 1], [0, 5], [1, 7], [3, 2], [3, 8], [
+        3, 4], [4, 8], [6, 0], [6, 1], [8, 2], [9, 4]]
     V = 10
     topologicalSort(edges, V)
 
 
 if __name__ == "__main__":
     driver()
-
-    # test change
